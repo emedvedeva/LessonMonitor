49b7d41e9bf0154dd053cd5adb0b3e307bd355cc
mevgenka <mevgenka@gmail.com>
2021-06-08 11:03:27 +0300

    Добавила класс для получения информации о моделях.

---
+++ LessonMonitor/LessonMonitor.API/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.API
+{
+    public class UserModel
+    {
+        public string Title { get; set; }
+        public List<string> Properties { get; set; }
+        pu
+    }
+}
