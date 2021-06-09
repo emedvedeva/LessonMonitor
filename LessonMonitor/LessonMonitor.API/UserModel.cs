e720bd04edfa07c30117f424250f7581dbbdbe29
mevgenka <mevgenka@gmail.com>
2021-06-09 22:10:37 +0300

    Добавила класс для получения моделей через рефлексию

--- LessonMonitor/LessonMonitor.API/UserModel.cs
+++ LessonMonitor/LessonMonitor.API/UserModel.cs
@@ -1,14 +1,43 @@
-﻿using System;
+﻿using LessonMonitor.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace LessonMonitor.API
 {
-    public class UserModel
+    public class UserModel<T>
     {
         public string Title { get; set; }
         public List<string> Properties { get; set; }
-        pu
+
+        public UserModel(T model)
+        {
+            Title = GetClassTitle(model);
+            Properties = GetClassProperties(model);
+        }
+
+        private string GetClassTitle (T model)
+        {
+            Type t = model.GetType();
+            string result = t.Name;
+
+            return result;
+        }
+
+        private List<string> GetClassProperties(T model)
+        {
+            Type t = model.GetType();
+            PropertyInfo[] properties = t.GetProperties();
+
+            var result = new List<string>();
+
+            foreach (var property in properties)
+            {
+                result.Add(property.Name);
+            }
+            return result;
+        }
     }
 }
